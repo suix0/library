97e69d6900b3f8cf11705589b13d989e105605d7
bagasbas.nethangabriel <bagasbas.nethangabriel@gmail.com>
2024-03-25 05:02:54 +0000

    added content to the file

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [
+    {title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: '295', read: true},
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBook() {
+    const container = document.querySelector(".book-container");
+}
