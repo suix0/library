a1c854dbc235a58f8508a10f81421138a86aa149
suix0 <112095627+suix0@users.noreply.github.com>
2024-03-29 12:00:38 +0800

    implemented and designed a dialog modal

--- script.js
+++ script.js
@@ -9,10 +9,30 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function addToLibrary() {
+    const book = new Book()
 }
 
 function displayBook() {
+    // container for the book
     const container = document.querySelector(".book-container");
+
+    // create necessary HTML elements for book display
+
+    // loop through the library array and add the values to the HTML elements
+
+    // push the book to the container
 }
+
+
+// Open and Close Dialog modal
+const myModal = document.getElementById('modal');
+const modalClose = document.querySelector('.close');
+
+function displayModal() {
+    myModal.showModal();
+}
+
+modalClose.addEventListener('click', () => myModal.close());
+
+
