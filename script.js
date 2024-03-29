53602271b6e01d1b48710686f6b5a358ecfec8cd
suix0 <112095627+suix0@users.noreply.github.com>
2024-03-29 13:49:13 +0800

    implemented function to add book to library

--- script.js
+++ script.js
@@ -10,29 +10,54 @@ function Book(title, author, pages, read) {
 }
 
 function addToLibrary() {
-    const book = new Book()
+    const form = document.querySelector('form');
+    form.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        // Store form values
+        let title = document.querySelector('.title').value;
+        let author = document.querySelector('.author').value;
+        let pages = document.querySelector('.pages').value;
+        let read = document.querySelector('.read').checked;
+
+        const book = new Book(title, author, pages, read);
+
+        // Push book to the library
+        myLibrary.push(book);
+        const myModal = document.getElementById('modal');
+        myModal.close();
+        console.log(myLibrary);
+        // Clear text content pf input after closing modal
+        form.reset();
+    })
 }
+
 
 function displayBook() {
     // container for the book
     const container = document.querySelector(".book-container");
-
+    
     // create necessary HTML elements for book display
-
+    
     // loop through the library array and add the values to the HTML elements
-
+    
     // push the book to the container
 }
 
 
-// Open and Close Dialog modal
-const myModal = document.getElementById('modal');
-const modalClose = document.querySelector('.close');
-
 function displayModal() {
-    myModal.showModal();
+    // Open and Close Dialog modal
+    const myModal = document.getElementById('modal');
+    const openModal = document.querySelector('.openModal');
+    const modalClose = document.querySelector('.close');
+    openModal.addEventListener('click', () => myModal.showModal());
+
+    modalClose.addEventListener('click', () => myModal.close());
 }
 
-modalClose.addEventListener('click', () => myModal.close());
+addToLibrary();
+displayModal();
 
 
+
+ 
